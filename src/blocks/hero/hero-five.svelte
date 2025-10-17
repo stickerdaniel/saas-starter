e64f26f0de5ca057d065a96c2a338e4640ae0272
Daniel Sticker <daniel.sticker@rwth-aachen.de>
2025-10-17 12:05:42 +0200

    fix(hero): fix demo button clickable area and update integration link to use correct domain

--- src/blocks/hero/hero-five.svelte
+++ src/blocks/hero/hero-five.svelte
@@ -28,11 +28,10 @@
 						<Button
 							size="lg"
 							variant="ghost"
+							href="#link"
 							class="h-12 rounded-full px-5 text-base hover:bg-zinc-950/5 dark:hover:bg-white/5"
 						>
-							<a href="#link">
-								<span class="text-nowrap">Request a demo</span>
-							</a>
+							<span class="text-nowrap">Request a demo</span>
 						</Button>
 					</div>
 				</div>
