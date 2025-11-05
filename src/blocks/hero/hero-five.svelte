0e19c3597d8dee9b1f926b874762687bd9d04548
Daniel Sticker <daniel.sticker@rwth-aachen.de>
2025-11-05 02:02:55 +0100

    feat(hero): add Rive animation support with new RiveBackground component and integrate into hero section

--- src/blocks/hero/hero-five.svelte
+++ src/blocks/hero/hero-five.svelte
@@ -4,11 +4,17 @@
 	import ProgressiveBlur from '../magic/ProgressiveBlur.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import ChevronRight from '@lucide/svelte/icons/chevron-right';
+	import RiveBackground from '$lib/components/RiveBackground.svelte';
 </script>
 
-<main class="overflow-x-hidden">
-	<section>
-		<div class="py-24 md:pb-32 lg:pt-48 lg:pb-36">
+<main>
+	<section class="relative flex items-center justify-center">
+		<RiveBackground
+			src="/animations/spring-demo.riv"
+			stateMachine="Motion"
+			className="lg:left-[48%] xl:left-[55%] -bottom-2/5 lg:-bottom-1/6 w-[600px] h-[600px] lg:w-[700px] lg:h-[700px]"
+		/>
+		<div class="relative py-24 md:pb-32 lg:pt-48 lg:pb-36">
 			<div class="relative mx-auto flex max-w-6xl flex-col px-6 lg:block lg:px-12">
 				<div class="mx-auto max-w-lg text-center lg:ml-0 lg:max-w-full lg:text-left">
 					<h1 class="mt-8 max-w-4xl text-5xl text-balance md:text-6xl lg:mt-16 xl:text-7xl">
