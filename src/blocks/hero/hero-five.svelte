7fa64f42d13d11fe174aee8d4778a54b0ad1239a
Daniel Sticker <daniel.sticker@rwth-aachen.de>
2025-11-05 02:25:42 +0100

    fix(hero): restructure hero section layout and improve alignment of RiveBackground component

--- src/blocks/hero/hero-five.svelte
+++ src/blocks/hero/hero-five.svelte
@@ -8,13 +8,16 @@
 </script>
 
 <main>
-	<section class="relative flex items-center justify-center">
-		<RiveBackground
-			src="/animations/spring-demo.riv"
-			stateMachine="Motion"
-			className="lg:left-[48%] xl:left-[55%] -bottom-2/5 lg:-bottom-1/6 w-[600px] h-[600px] lg:w-[700px] lg:h-[700px]"
-		/>
-		<div class="relative py-24 md:pb-32 lg:pt-48 lg:pb-36">
+	<section class="relative">
+		<div class="flex items-center justify-center">
+			<RiveBackground
+				src="/animations/spring-demo.riv"
+				stateMachine="Motion"
+				className="lg:left-[45%] xl:left-[52%] -bottom-2/5 lg:-bottom-1/6 w-[600px] h-[600px] lg:w-[700px] lg:h-[700px]"
+			/>
+		</div>
+
+		<div class="py-24 md:pb-32 lg:pt-48 lg:pb-36">
 			<div class="relative mx-auto flex max-w-6xl flex-col px-6 lg:block lg:px-12">
 				<div class="mx-auto max-w-lg text-center lg:ml-0 lg:max-w-full lg:text-left">
 					<h1 class="mt-8 max-w-4xl text-5xl text-balance md:text-6xl lg:mt-16 xl:text-7xl">
