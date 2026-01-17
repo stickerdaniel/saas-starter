1c5e71f27df3adc92a6ae8e7f20a68edd0be5a05
Daniel Sticker <daniel.sticker@rwth-aachen.de>
2026-01-17 23:15:35 +0100

    feat(logo): replace lucide icon with masked logo svg

---
+++ src/lib/components/icons/logo.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { IconProps } from '@lucide/svelte';
+	import { cn } from '$lib/utils';
+
+	// Accept IconProps for compatibility with LucideIcon type, but only use class
+	let { class: className }: IconProps = $props();
+</script>
+
+<span
+	class={cn(
+		'inline-block bg-[var(--logo-color,currentColor)] [mask-image:url(/logo.svg)] [mask-size:contain] [mask-position:center] [mask-repeat:no-repeat]',
+		className
+	)}
+></span>
