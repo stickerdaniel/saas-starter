79d28916138f4300affdc2fc06bbb289e98976e6
Daniel Sticker <daniel.sticker@rwth-aachen.de>
2026-02-08 15:17:56 +0100

    fix(about): add and apply no-drag utility

--- src/blocks/team/team-two.svelte
+++ src/blocks/team/team-two.svelte
@@ -67,7 +67,7 @@
 				{#each members as member, index (member.name)}
 					<div class="group overflow-hidden">
 						<img
-							class="h-96 w-full rounded-md object-cover object-top grayscale transition-all duration-500 group-hover:h-[22.5rem] group-hover:rounded-xl hover:grayscale-0"
+							class="h-96 w-full rounded-md object-cover object-top grayscale transition-all duration-500 no-drag group-hover:h-[22.5rem] group-hover:rounded-xl hover:grayscale-0"
 							src={member.avatar}
 							alt="team member"
 							width="826"
@@ -94,7 +94,7 @@
 								<a
 									href={resolve(member.link)}
 									draggable={false}
-									class="group-hover:text-primary-600 dark:group-hover:text-primary-400 inline-block translate-y-8 text-sm tracking-wide opacity-0 transition-all duration-500 group-hover:translate-y-0 group-hover:opacity-100 hover:underline"
+									class="group-hover:text-primary-600 dark:group-hover:text-primary-400 inline-block translate-y-8 text-sm tracking-wide opacity-0 transition-all duration-500 no-drag group-hover:translate-y-0 group-hover:opacity-100 hover:underline"
 								>
 									<T keyName="team.linktree" />
 								</a>
