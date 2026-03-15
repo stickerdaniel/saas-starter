ff68d7875b5440a52374975c54ef9a4ef0018a3b
Daniel Sticker <daniel.sticker@rwth-aachen.de>
2026-03-15 20:24:16 +0100

    feat: Add SVG spotlight beam to dark mode Rive hero section

---
+++ src/lib/components/ui/spotlight/spotlight.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { cn } from '$lib/utils.js';
+
+	interface SpotlightProps {
+		className?: string;
+		fill?: string;
+	}
+
+	let { className, fill = 'white' }: SpotlightProps = $props();
+</script>
+
+<svg
+	class={cn(
+		'pointer-events-none absolute z-[1] h-[300%] w-[250%] animate-spotlight opacity-0',
+		className
+	)}
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 3787 2842"
+	fill="none"
+>
+	<g filter="url(#spotlight-filter)" transform="translate(3787,0) scale(-1,1)">
+		<ellipse
+			cx="1924.71"
+			cy="273.501"
+			rx="1924.71"
+			ry="273.501"
+			transform="matrix(-0.822377 -0.568943 -0.568943 0.822377 3631.88 2291.09)"
+			{fill}
+			fill-opacity="0.35"
+		></ellipse>
+	</g>
+	<defs>
+		<filter
+			id="spotlight-filter"
+			x="-500"
+			y="-500"
+			width="4787"
+			height="3842"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB"
+		>
+			<feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+			<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
+			<feGaussianBlur stdDeviation="151" result="effect1_foregroundBlur"></feGaussianBlur>
+		</filter>
+	</defs>
+</svg>
