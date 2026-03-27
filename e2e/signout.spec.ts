199811e09bfa8aa4fd6373e9256029b7f22303f3
Daniel Sticker <daniel.sticker@rwth-aachen.de>
2026-03-27 12:56:25 +0100

    fix(e2e): Restore signout test selectors from main

--- e2e/signout.spec.ts
+++ e2e/signout.spec.ts
@@ -8,8 +8,8 @@ test('signout works', async ({ page }) => {
 	await waitForAuthenticated(page);
 
 	// Click user menu and sign out
-	await page.locator('[data-sidebar="menu-button"][data-size="lg"]').click();
-	await page.getByText('Log out').click();
+	await page.locator('#user-menu-trigger').click();
+	await page.locator('[data-testid="logout-button"]').click();
 
 	// Should redirect away from app after logout (to home or signin page)
 	// With i18n, this could be /en, /en/signin, etc.
